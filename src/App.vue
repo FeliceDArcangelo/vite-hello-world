b4b0dda7ae85ac8fda33a9a5c144190c55cdfbb8
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-05-23 05:55:06 +0200

    vite hello

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import titleEx from './components/titleEx.vue';
+
+  export default{
+    components: {
+      titleEx,
+    }
+  }
+</script>
+
+<template>
+  <titleEx/>
+</template>
+
+<style scoped>
+
+</style>
